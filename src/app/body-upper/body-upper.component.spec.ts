ed8280aa5f761b805d5d2bcc9b199fb097a1442b
SUSHMADASCJ <51118772+SUSHMADASCJ@users.noreply.github.com>
2021-02-13 11:03:26 +0530

    first part of the landing page

---
+++ src/app/body-upper/body-upper.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BodyUpperComponent } from './body-upper.component';
+
+describe('BodyUpperComponent', () => {
+  let component: BodyUpperComponent;
+  let fixture: ComponentFixture<BodyUpperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BodyUpperComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BodyUpperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/body-upper/body-upper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {AppComponent} from '../app.component';
+@Component({
+  selector: 'app-body-upper',
+  templateUrl: './body-upper.component.html',
+  styleUrls: ['./body-upper.component.css']
+})
+export class BodyUpperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
